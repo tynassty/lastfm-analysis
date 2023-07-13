c41df8ee0a4885585e59a43e8b272d46806d13d8
nasst <nasst@union.edu>
2023-07-12 22:35:29 -0400

    recommitting with scrobble data

--- lastfm_reader.py
+++ lastfm_reader.py
@@ -7,7 +7,8 @@ def read_scrobbles(scrobbles_file):
     next(f)
     for line in f:
         line = line.split('",')
-        scrobbles.append(Scrobble(int(line[0][1:]), line[1][1:], line[2][1:], line[3][1:], line[4][1:], line[5][1:], line[6][1:], line[7][1:-1]))
+        scrobbles.append(Scrobble(int(line[0][1:]), line[1][1:], line[2][1:], line[3][1:],
+                                  line[4][1:], line[5][1:], line[6][1:], line[7][1:-1]))
     scrobbles = sorted(scrobbles)
     return scrobbles
 
